e030ed6b17844f5af7882862c97211452e521a48
Ethiraj Haribabu <ethiraj.haribabu@photon.com>
2024-09-25 16:23:41 +0530

    Git Repo Path Changed

--- auto-push.js
+++ auto-push.js
@@ -2,7 +2,7 @@ const simpleGit =require('simple-git');
 const path = require('path');
 const dir = 'src/featurefile'
  
-const repoPath =path.join(dir,'https://github.com/Yugendra12/mygit-autopush.git');
+const repoPath =path.join(dir,'https://github.com/RoomNo55/WDIO.js.git');
 const git =simpleGit(repoPath);
 async function autoPush(){
  
